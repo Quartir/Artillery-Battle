e5222c12124469cddd8a8ccee3462afd40aaf55a
Quartir <57145428+Quartir@users.noreply.github.com>
2023-07-11 22:42:24 -0400

    Update mapresetfunction.lua

--- mapresetfunction.lua
+++ mapresetfunction.lua
@@ -50,31 +50,10 @@ while true do
 end
 ]]
 
-function module.MapReset()
-	--workspace:WaitForChild("Enviroment1"):Destroy()
-	--wait(0.2)
-	--local clonedEnviroment1 = game.ServerStorage.Enviroment1:Clone()
-	--clonedEnviroment1.Parent = game.Workspace
-	--wait(0.2)
-	--workspace:WaitForChild("Enviroment2"):Destroy()
-	--wait(0.2)
-	--local clonedEnviroment2 = game.ServerStorage.Enviroment2:Clone()
-	--clonedEnviroment2.Parent = game.Workspace
-end
-
 function module.MapResetGui()
-	workspace:WaitForChild("Enviroment1"):Destroy()
-	wait(0.2)
-	local clonedEnviroment1 = game.ServerStorage.Enviroment1:Clone()
-	clonedEnviroment1.Parent = game.Workspace
-	wait(0.2)
-	workspace:WaitForChild("Enviroment2"):Destroy()
-	wait(0.2)
-	local clonedEnviroment2 = game.ServerStorage.Enviroment2:Clone()
-	clonedEnviroment2.Parent = game.Workspace
-	game.ServerScriptService.MapResettimer.Enabled = false
-	wait(1)
-	game.ServerScriptService.MapResettimer.Enabled = true
+	local clone = game.ServerStorage.Enviroment:Clone()
+	game.Workspace.Enviroment:Destroy()
+	clone.Parent = game.Workspace
 end
 
 function tblremovekey(tbl, key)
